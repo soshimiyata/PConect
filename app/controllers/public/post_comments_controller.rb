f90275552b7ae41759fade9abd33c7f74506675d
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-03-12 05:27:52 +0000

    add admin post-comments destroy and change Views

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,21 +1,29 @@
 class Public::PostCommentsController < ApplicationController
 
-  def create
-    post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
-  end
+def create
+  @post = Post.find(params[:post_id])
+  comment = current_user.post_comments.new(post_comment_params)
+  comment.post_id = @post.id
+  comment.save
+  @post_comment = PostComment.new
+  # redirect_to request.referer
+end
 
-  def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
-  end
+def show
+  @post = Post.find(params[:id])
+  @post_comment = PostComment.new
+end
 
-  private
+def destroy
+  PostComment.find(params[:id]).destroy
+  @post = Post.find(params[:post_id])
+  # redirect_to post_path(params[:post_id])
+end
 
-  def post_comment_params
-    params.require(:post_comment).permit(:comment)
-  end
+private
+
+def post_comment_params
+  params.require(:post_comment).permit(:comment)
+end
+
 end
