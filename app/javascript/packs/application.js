2f9e7c918fa35d8612413dbaed875683ab60fa92
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-03-14 10:44:46 +0000

    change application.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ import "channels";
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application"; 
+import "../stylesheets/application";
 require("jquery")
 import "cocoon";
 import "script.js"
