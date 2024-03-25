64a9e047ee32b79b7f836aa95590b6a58159dc95
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-03-25 06:39:29 +0000

    add user_state

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :user_state, only: [:create]
 
   def after_sign_in_path_for(resource)
     root_path
