7ad79dac2fdb56c0d45aeba60fff721d7f310edf
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-03-28 09:11:13 +0000

    fix views

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,4 @@ Rails.application.routes.draw do
     resources :users, only: [:index,:show,:edit,:update,:destroy]
     resources :posts, only: [:index,:show,:destroy]
   end
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
