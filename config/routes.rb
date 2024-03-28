2093cadb001d2d88f651d5318179ebdf22d17592
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-03-28 02:11:08 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
+
   # 顧客用
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
@@ -53,5 +42,6 @@ Rails.application.routes.draw do
     resources :users, only: [:index,:show,:edit,:update,:destroy]
     resources :posts, only: [:index,:show,:destroy]
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
