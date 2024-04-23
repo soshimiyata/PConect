065e6d07dcbdd5398ceccd8286aba417271d0342
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-04-23 06:38:27 +0000

    fix top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,9 @@
 class Public::HomesController < ApplicationController
   def top
     @posts = Post.page(params[:page]).per(6)
+    @posts_all = Post.all
     # 新規投稿4件をスライドショー
-    @new_posts = @posts.sort_by { |post| post.created_at }.reverse.first(4)
+    @new_posts = @posts_all.sort_by { |post| post.created_at }.reverse.first(4)
   end
 
   def about
