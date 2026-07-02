1e3afdd64f88b2fb65d4ac562955784ff82a4c9c
soshimiyata <gchuangshi@gmail.com>
2026-07-02 23:18:40 +0900

    search function edit

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,14 +2,13 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
-		@model = params[:model]
-		@content = params[:content]
-		@method = params[:method]
-		# 選択したモデルに応じて検索を実行
-		if @model == 'user'
-			@records = User.search_for(@content, @method)
-		else
-			@records = Post.search_for(@content, @method)
-		end
-	end
+    @model = params[:model]
+    @content = params[:content]
+
+    if @model == "user"
+      @records = User.search_for(@content)
+    else
+      @records = Post.search_for(@content)
+    end
+  end
 end
