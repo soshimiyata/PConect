008a50cbb95b2e192a27c776641b03be42a5de56
soshimiyata <gchuangshi@gmail.com>
2026-07-18 23:48:44 +0900

    Optimize SQL queries

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,11 @@
 class Public::HomesController < ApplicationController
   def top
-    @posts = Post.page(params[:page]).per(6)
-    @posts_all = Post.all
+    @posts = Post.includes(:user, :post_images, :tags).order(created_at: :desc)
+                .page(params[:page]).per(6)
     # 新規投稿4件をスライドショー
-    @new_posts = @posts_all.sort_by { |post| post.created_at }.reverse.first(4)
+    @new_posts = Post.includes(:post_images)
+                    .order(created_at: :desc)
+                    .limit(4)
   end
 
   def about
