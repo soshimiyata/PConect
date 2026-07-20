75c241cbc50ab5f743bcbbd4126ee3d2b67f9f5d
soshimiyata <gchuangshi@gmail.com>
2026-07-20 17:00:17 +0900

    db debug

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,5 @@
 class Public::HomesController < ApplicationController
   def top
-    @posts = Post.includes(:user, :post_images, :tags).order(created_at: :desc)
-                .page(params[:page]).per(6)
     # 新規投稿4件をスライドショー
     @new_posts = Post.includes(:post_images)
                     .order(created_at: :desc)
